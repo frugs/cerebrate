a4cf49056b52501ec980370d205a2ba0a0e8c913
Hugo Wainwright <wainwrighthugo@gmail.com>
2017-05-16 02:49:10 +0100

    move autocomplete installation to .bash_profile

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
         if sys.platform == "darwin" or sys.platform.startswith("linux"):
-            os.system('''echo 'eval "$(_CEREBRATE_COMPLETE=source cerebrate)"' >> ~/.bashrc''')
+            os.system('''echo 'eval "$(_CEREBRATE_COMPLETE=source cerebrate)"' >> ~/.bash_profile''')
 
         install.run(self)
 
 setup(
     name='cerebrate',
-    version='0.0.5',
+    version='0.0.6',
     description='Offline Replay Manager for StarCraft II',
     author='Hugo Wainwright',
     author_email='wainwrighthugo@gmail.com',
     url='https://github.com/frugs/cerebrate',
-    download_url='https://github.com/frugs/cerebrate/tarball/0.0.5',
+    download_url='https://github.com/frugs/cerebrate/tarball/0.0.6',
     packages=find_packages(),
     py_modules=['cerebrate'],
     include_package_data=True,
