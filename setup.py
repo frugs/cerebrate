6a4e167be8697d791e6c1c46cdf4db1316b8665b
Hugo Wainwright <wainwrighthugo@gmail.com>
2017-05-16 02:54:34 +0100

    use .bashrc in linux, .profile on mac

--- setup.py
+++ setup.py
@@ -7,19 +7,21 @@ from setuptools.command.install import install
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
-        if sys.platform == "darwin" or sys.platform.startswith("linux"):
-            os.system('''echo 'eval "$(_CEREBRATE_COMPLETE=source cerebrate)"' >> ~/.bash_profile''')
+        if sys.platform == "darwin":
+            os.system('''echo 'eval "$(_CEREBRATE_COMPLETE=source cerebrate)"' >> ~/.profile''')
+        elif sys.platform.startswith("linux"):
+            os.system('''echo 'eval "$(_CEREBRATE_COMPLETE=source cerebrate)"' >> ~/.bashrc''')
 
         install.run(self)
 
 setup(
     name='cerebrate',
-    version='0.0.6',
+    version='0.0.7',
     description='Offline Replay Manager for StarCraft II',
     author='Hugo Wainwright',
     author_email='wainwrighthugo@gmail.com',
     url='https://github.com/frugs/cerebrate',
-    download_url='https://github.com/frugs/cerebrate/tarball/0.0.6',
+    download_url='https://github.com/frugs/cerebrate/tarball/0.0.7',
     packages=find_packages(),
     py_modules=['cerebrate'],
     include_package_data=True,
