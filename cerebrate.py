3b1e93c3fb10e36eeb15f405bdefd0d3b8bbae00
Hugo Wainwright <wainwrighthugo@gmail.com>
2017-05-16 01:30:49 +0100

    implemented tag frequency command

--- cerebrate.py
+++ cerebrate.py
@@ -42,16 +42,34 @@ def remove_tag(most_recent_replay, replay, tags):
     _replay_manager.untag_replay(replay, list(tags))
 
 
-@click.command(name="search")
+@click.command(name="query-replays")
 @click.option("--source-list", "-l", type=click.File('r'), help="File containing list of replays files to query from")
 @click.option("--source-dir", "-d", type=click.Path(), help="Directory containing replay files to query from")
 @click.option("--match-any-tag", type=bool, is_flag=True, default=False,
               help="How to match tags on replays. Default is to match all tags.")
-@click.option("--inverse", type=bool, is_flag=True, help="Search for replays not matching the tag")
+@click.option("--inverse", type=bool, is_flag=True, help="Search for replays not matching the tags")
 @click.argument('tags', nargs=-1, type=str)
-def search(source_list, source_dir, match_any_tag, inverse, tags):
-    source_replays = []
+def query_replays(source_list, source_dir, match_any_tag, inverse, tags):
+    source_replays = find_and_add_source_replays(source_list, source_dir)
 
+    replays_paths = _replay_manager.query_replays(match_any_tag, inverse, source_replays, list(tags))
+    for path in replays_paths:
+        click.echo(path)
+
+
+@click.command(name="tag-frequency")
+@click.option("--source-list", "-l", type=click.File('r'), help="File containing list of replays files to query from")
+@click.option("--source-dir", "-d", type=click.Path(), help="Directory containing replay files to query from")
+def tag_frequency(source_list, source_dir):
+    source_replays = find_and_add_source_replays(source_list, source_dir)
+
+    tag_frequencies = _replay_manager.tag_frequency(source_replays)
+    for tag, frequency in tag_frequencies:
+        click.echo(tag + " " + str(frequency))
+
+
+def find_and_add_source_replays(source_list, source_dir):
+    source_replays = []
     if source_list:
         while True:
             source = source_list.readline()
@@ -59,20 +77,19 @@ def search(source_list, source_dir, match_any_tag, inverse, tags):
                 source_replays.append(source)
             else:
                 break
-
     if source_dir:
         if not os.path.isdir(source_dir):
             raise click.UsageError("source dir should be a directory!")
 
         source_replays.extend(glob.glob(os.path.join(source_dir, "*.SC2Replay")))
-
-    replays_paths = _replay_manager.query_replays(match_any_tag, inverse, source_replays, list(tags))
-    for path in replays_paths:
-        click.echo(path)
+    for replay in source_replays:
+        _replay_manager.tag_replay(replay, [])
+    return source_replays
 
 cli.add_command(add_tag)
 cli.add_command(remove_tag)
-cli.add_command(search)
+cli.add_command(query_replays)
+cli.add_command(tag_frequency)
 
 if __name__ == '__main__':
     cli()
--- replaymanager/replaymanager.py
+++ replaymanager/replaymanager.py
@@ -23,20 +23,6 @@ def _remove_tags(tags: list):
     return transform
 
 
-def _match_all_tags(tags: list):
-    def match(replay_tags):
-        return bool(all(tag in replay_tags for tag in tags))
-
-    return match
-
-
-def _match_any_tags(tags: list):
-    def match(replay_tags):
-        return bool(any(tag in replay_tags for tag in tags))
-
-    return match
-
-
 def _hash_replay(replay_path):
     with open(replay_path, "rb") as replay_file:
         hash_calculator = hashlib.sha256()
@@ -67,8 +53,7 @@ class ReplayManager:
     def tag_replay(self, replay_path: str, tags: list):
         replay_hash = _hash_replay(replay_path)
 
-        replay = tinydb.Query()
-        replay_updated = self.__db.update(_add_tags(tags), replay['hash'] == replay_hash)
+        replay_updated = self.__db.update(_add_tags(tags), tinydb.Query()['hash'] == replay_hash)
         if not replay_updated:
             replay_canonical_path = os.path.join(self.__replay_archive_path, replay_hash + ".SC2Replay")
             shutil.copyfile(replay_path, replay_canonical_path)
@@ -77,20 +62,28 @@ class ReplayManager:
     def untag_replay(self, replay_path: str, tags: list):
         replay_hash = _hash_replay(replay_path)
 
-        replay = tinydb.Query()
-        self.__db.update(_remove_tags(tags), replay['hash'] == replay_hash)
+        self.__db.update(_remove_tags(tags), tinydb.Query()['hash'] == replay_hash)
 
     def query_replays(self, match_any_tag: bool, inverse: bool, replays_to_query_from: list, tags: list) -> list:
 
-        replay_hashes = map(_hash_replay, replays_to_query_from)
+        replay_hashes = list(map(_hash_replay, replays_to_query_from))
 
         replay = tinydb.Query()
 
         query = replay['tags'].any(tags) if match_any_tag else replay['tags'].all(tags)
         query = ~query if inverse else query
-        query = query | replay['hash'].test(lambda x: x in replay_hashes) if replay_hashes else query
+        query = query & replay['hash'].test(lambda x: x in replay_hashes) if replay_hashes else query
 
         matches = self.__db.search(query)
 
         return [match['canonical_path'] for match in matches]
 
+    def tag_frequency(self, source_replays: list) -> list:
+        if source_replays:
+            replay_hashes = list(map(_hash_replay, source_replays))
+            matches = self.__db.search(tinydb.Query()['hash'].test(lambda x: x in replay_hashes))
+        else:
+            matches = self.__db.all()
+
+        tags = [tag for match in matches for tag in match['tags']]
+        return list(sorted([(tag, tags.count(tag)) for tag in set(tags)], key=lambda x: x[1], reverse=True))
