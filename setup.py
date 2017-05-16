ca955b69f47cfe489fe93ed0678d58e9e6aeedd2
Hugo Wainwright <wainwrighthugo@gmail.com>
2017-05-16 02:02:58 +0100

    setuptools update

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='cerebrate',
-    version='0.0.1',
+    version='0.0.3',
     description='Offline Replay Manager for StarCraft II',
     author='Hugo Wainwright',
     author_email='wainwrighthugo@gmail.com',
     url='https://github.com/frugs/cerebrate',
-    download_url='https://github.com/frugs/cerebrate/tarball/0.0.1',
-    packages=['replaymanager', 'replaysearch'],
+    download_url='https://github.com/frugs/cerebrate/tarball/0.0.3',
+    packages=find_packages(),
     py_modules=['cerebrate'],
+    include_package_data=True,
     install_requires=[
         'tinydb',
         'click',
