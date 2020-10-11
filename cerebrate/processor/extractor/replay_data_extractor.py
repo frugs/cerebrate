ead2d40f9de9ee658026f063645a51a6e527098d
Hugo Wainwright <wainwrighthugo@gmail.com>
2020-10-11 15:31:39 +0100

    refactored replay data extractor

--- cerebrate/processor/extractor/replay_data_extractor.py
+++ cerebrate/processor/extractor/replay_data_extractor.py
@@ -1,16 +1,79 @@
-from typing import Final
+from typing import Final, Optional
 
 import sc2reader
+import sc2reader.objects
 import sc2reader.resources
 
 from cerebrate.core import Replay
 
 
 class ReplayDataExtractor:
+    _player_team: Optional[sc2reader.objects.Team]
+    _opponent_team: Optional[sc2reader.objects.Team]
+    _player: Optional[sc2reader.objects.Player]
+    _opponent: Optional[sc2reader.objects.Player]
+
     replay_info: Final[Replay]
     source_replay_data: Final[sc2reader.resources.Replay]
 
     def __init__(self, replay: Replay):
+        self._player_team = None
+        self._opponent_team = None
+        self._player = None
+        self._opponent = None
+
         self.replay_info = replay
         # noinspection PyUnresolvedReferences
         self.source_replay_data = sc2reader.load_replay(replay.path, load_level=4)
+
+    @property
+    def player_team(self) -> Optional[sc2reader.objects.Team]:
+        if self._player_team:
+            return self._player_team
+
+        if (
+            self.replay_info.player_team is None
+            or not len(self.source_replay_data.teams) > self.replay_info.player_team
+        ):
+            return None
+
+        self._player_team = self.source_replay_data.teams[self.replay_info.player_team]
+        return self._player_team
+
+    @property
+    def opponent_team(self) -> Optional[sc2reader.objects.Team]:
+        if self._opponent_team:
+            return self._opponent_team
+
+        if (
+            self.replay_info.opponent_team is None
+            or not len(self.source_replay_data.teams) > self.replay_info.opponent_team
+        ):
+            return None
+
+        self._opponent_team = self.source_replay_data.teams[
+            self.replay_info.opponent_team
+        ]
+        return self._opponent_team
+
+    @property
+    def player(self) -> Optional[sc2reader.objects.Player]:
+        if self._player:
+            return self._player
+
+        if not self.player_team or len(self.player_team.players) != 1:
+            return None
+
+        self._player = self.player_team.players[0]
+        return self._player
+
+    @property
+    def opponent(self) -> Optional[sc2reader.objects.Player]:
+        if self._opponent:
+            return self._opponent
+
+        if not self.opponent_team or len(self.opponent_team.players) != 1:
+            return None
+
+        self._opponent = self.opponent_team.players[0]
+        return self._opponent
--- cerebrate/processor/generator/race_tag_generator.py
+++ cerebrate/processor/generator/race_tag_generator.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Iterable
+from typing import Callable, Iterable, List
 
 import sc2reader.objects
 import sc2reader.resources
@@ -42,42 +42,17 @@ class RaceTagGenerator(TagGenerator):
         self, replay: Replay, replay_data_extractor: ReplayDataExtractor
     ) -> List[str]:
         tags = []
-        if (
-            replay.player_team is None
-            or not len(replay_data_extractor.source_replay_data.teams) > replay.player_team
-        ):
+
+        if not replay_data_extractor.player:
             return tags
 
-        sc2reader_player_team: sc2reader.objects.Team = replay_data_extractor.source_replay_data.teams[
-            replay.player_team
-        ]
-        if len(sc2reader_player_team.players) != 1:
+        player_race = replay_data_extractor.player.play_race.lower()
+        tags.append(Replay.create_player_tag(player_race))
+
+        if not replay_data_extractor.opponent:
             return tags
 
-        player_team_sole_player: sc2reader.objects.Player = (
-            sc2reader_player_team.players[0]
-        )
-        player_race_tag = Replay.create_player_tag(
-            player_team_sole_player.pick_race.lower()
-        )
-        tags.append(player_race_tag)
-
-        if (
-            replay.opponent_team is None
-            or not len(replay_data_extractor.source_replay_data.teams) > replay.opponent_team
-        ):
-            return tags
-
-        sc2reader_opp_team: sc2reader.objects.Team = replay_data_extractor.source_replay_data.teams[
-            replay.opponent_team
-        ]
-        if len(sc2reader_opp_team.players) != 1:
-            return tags
-
-        opp_team_sole_player: sc2reader.objects.Player = sc2reader_opp_team.players[0]
-        opp_race_tag = Replay.create_opponent_tag(
-            opp_team_sole_player.pick_race.lower()
-        )
-        tags.append(opp_race_tag)
+        opponent_race = replay_data_extractor.opponent.play_race.lower()
+        tags.append(Replay.create_opponent_tag(opponent_race))
 
         return tags
