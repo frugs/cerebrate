a2936a419ebc0127e54c6e75cf4f7c98d908d893
Hugo Wainwright <wainwrighthugo@gmail.com>
2020-10-12 00:21:53 +0100

    add random tag generator

--- cerebrate/processor/generator/generators.py
+++ cerebrate/processor/generator/generators.py
@@ -2,9 +2,15 @@ from typing import List
 
 from .matchup_tag_generator import MatchupTagGenerator
 from .race_tag_generator import RaceTagGenerator
+from .random_tag_generator import RandomTagGenerator
 from .result_tag_generator import ResultTagGenerator
 from .tag_generator import TagGenerator
 
 
 def create_tag_generators() -> List[TagGenerator]:
-    return [RaceTagGenerator(), ResultTagGenerator(), MatchupTagGenerator()]
+    return [
+        RaceTagGenerator(),
+        RandomTagGenerator(),
+        ResultTagGenerator(),
+        MatchupTagGenerator(),
+    ]
---
+++ cerebrate/processor/generator/random_tag_generator.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from cerebrate.core import Replay
+from cerebrate.processor.extractor import ReplayDataExtractor
+
+from .tag_generator import TagGenerator
+
+PLAYER_RANDOM_TAG = Replay.create_player_tag("random")
+OPPONENT_RANDOM_TAG = Replay.create_opponent_tag("random")
+
+
+class RandomTagGenerator(TagGenerator):
+    def tags_to_remove(self) -> List[str]:
+        return [PLAYER_RANDOM_TAG, OPPONENT_RANDOM_TAG]
+
+    def generate_tags(
+        self, replay: Replay, replay_data_extractor: ReplayDataExtractor
+    ) -> List[str]:
+        tags = []
+
+        if (
+            not replay_data_extractor.player
+            or not replay_data_extractor.player.pick_race
+        ):
+            return tags
+
+        if replay_data_extractor.player.pick_race == "Random":
+            tags.append(PLAYER_RANDOM_TAG)
+
+        if (
+            not replay_data_extractor.opponent
+            or not replay_data_extractor.opponent.pick_race
+        ):
+            return tags
+
+        if replay_data_extractor.opponent.pick_race == "Random":
+            tags.append(OPPONENT_RANDOM_TAG)
+
+        return tags
